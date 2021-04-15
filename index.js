01c56a79670417799a23dfe4d13375aefdf8d905
perclf <lrf@jhf.name>
2021-04-15 14:40:04 +0200

    Added command manager

--- index.js
+++ index.js
@@ -2,14 +2,36 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./config.json");
 const https = require("https");
+const fs = require("fs");
+
+//Loading commands
+bot.commandPrefix = ".";
+bot.commands = new Discord.Collection();
+fs.readdir("./cmds/", (err, files) => {
+	if(err) console.error(err);
+	
+	let jsFiles = files.filter(f => f.split(".").pop() === "js");
+	if (jsFiles.length == 0) return console.log("No cmds loaded");
+	
+	jsFiles.forEach((f, i) => {
+		let props = require(`./cmds/${f}`);
+		console.log(`${i+1}: ${f} loaded!`);	
+		bot.commands.set(props.info.name, props);
+	});
+});
 
 async function playSound(channel) {
 	const connection = await channel.join()
 	const dispatcher = connection.play("./doep.oga");
 }
 
+bot.on("ready", async () => {
+	console.log("bot ready");
+});
+
 bot.on("message", async msg => {
-	if (msg.author.bot) return;
+	if(msg.author.bot) return; 
+	
 	if (msg.content.includes("yo momma")) {
 		https.get('https://api.yomomma.info/', (response) => {
   			let json = '';
@@ -28,6 +50,14 @@ bot.on("message", async msg => {
   				console.log("Error: " + error.message);
 			});
 	}
+
+	const messageArray = msg.content.split(" ");
+	const command = messageArray[0].slice(bot.commandPrefix.length);
+	const args = messageArray.slice(1);
+
+	if(!msg.content.startsWith(bot.commandPrefix)) return;
+	cmd = bot.commands.get(command);
+	if(cmd) cmd.run(bot, msg, args);	
 });
 
 bot.on("voiceStateUpdate", (oldMemberState, newMemberState) => {
